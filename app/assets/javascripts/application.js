5c0f727146b05b305e9347a0ad84b191c2bdb58a
Oscar Fabra <oscarfabra@gmail.com>
2018-09-15 01:37:17 -0500

    Reorder Javascript to avoid bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
